b5726bc50cd21dd56ee4d8394b388a5990c7ee88
Claudius Mbemba <mbembac@gmail.com>
2015-04-18 17:16:05 -0400

    edited routes and have front page connected to save user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,17 @@
 class WelcomeController < ApplicationController
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render("SAVED")
-    else
-      render("welcome")
-    end
+    # @user = User.new(user_params)
+    # if @user.save
+    #   render("SAVED")
+    # else
+    #   render("welcome")
+    # end
+    render("test")
   end
+
   def index
+
   end
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match ":controller:/(:action:/(:id))", :via => :get
+  match ":controller(/:action(/:id))", :via => [:get, :post]
 
   root 'welcome#index'
 
