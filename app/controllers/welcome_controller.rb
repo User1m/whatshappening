48c12d424d830d78e38d7f30ab34b8be2948e7f4
Jae Lee <jae@dustral.com>
2015-04-18 17:42:38 -0400

    Finish implementing sign up form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,19 @@
 class WelcomeController < ApplicationController
-
   def create
-    # @user = User.new(user_params)
-    # if @user.save
-    #   render("SAVED")
-    # else
-    #   render("welcome")
-    # end
-    render("test")
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def index
-
+    @user = User.new
   end
 
   def user_params
-    params.requrie(:user).permit(:email, :phone)
+    params.require(:user).permit(:email, :phone_no)
   end
 end
