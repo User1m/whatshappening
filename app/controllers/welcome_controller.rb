d3a56bc667e6144ca52820e2b4ea373c537649a7
Claudius Mbemba <mbembac@gmail.com>
2015-04-18 17:54:47 -0400

    setting up user verification

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,14 +7,36 @@ class WelcomeController < ApplicationController
     # else
     #   render("welcome")
     # end
-    render("test")
+    redirect_to(:action => "test", :email => params[:email], :phone => params[:phone])
+    # redirect_to(:action => "verify", :email =>params[:email], :phone => params[:phone])
   end
 
   def index
 
   end
 
+  def test
+    @email = params[:email]
+    @phone = params[:phone]
+  end
+
   def user_params
     params.requrie(:user).permit(:email, :phone)
+  end
+
+  private
+  # will contact user via ezText and ask quesitonaire
+  def verify
+    # get user phone
+    phone = params[:phone]
+
+    # build message
+    options = {:subject => "Welcome to What'sHappening!", :phonenumber => "7406410248", :message => "We'd like to get you up and running but first we have 3 simple questions just reply via text\n1. What is your name?\n2. What is your zipcode?\n3. How old is your child?\n4. What is your child interested in? Reply with either 'Arts','Sports', or 'Volunteering'. You will be receiving event texts starting this weekend"}
+
+     options = {:subject => "Welcome to What'sHappening!", :phonenumber => "7406410248", :message => "Test"}
+
+    # send text
+    Eztexting::Sms.single(options)
+
   end
 end
