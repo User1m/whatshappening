5210987bf50badd0af9289911b8e938d05be04d0
Jae Lee <jae@dustral.com>
2015-04-19 13:14:05 -0400

    Test messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,22 @@ class MessagesController < ApplicationController
 
     @client = Twilio::REST::Client.new ENV['TWILIOSID'], ENV['TWILIOTOKEN']
 
+    keyword = message.split.first.downcase
+    body = ""
+
+    case keyword
+    when "events"
+      body = "Here are some events:"
+    when "help"
+      body = "Do this and this."
+    else
+      body = "Couldn't process."
+    end
+
     @client.messages.create(
       from: ENV['TWILIOPHONE'],
       to: sender,
-      body: "you tried"
+      body: body
     )
 
     render nothing: true
