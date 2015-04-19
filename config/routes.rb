cec308ed64e20fcac18937ae2e33f0448f8c1271
Jae Lee <jae@dustral.com>
2015-04-18 22:34:41 -0400

    Add Messages controller for incoming texts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'success' => 'welcome#success'
+  get 'forward' => 'messages#forward', as: :forward
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
