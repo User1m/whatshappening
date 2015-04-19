e692ef5d1b3ee2f7525bd6dddd0077353a100603
Jae Lee <jae@dustral.com>
2015-04-19 00:28:57 -0400

    Remove unused code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -30,19 +30,4 @@ class WelcomeController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :phone_no)
   end
-
-  # will contact user via ezText and ask quesitonaire
-  def verify
-    # get user phone
-    phone = params[:phone]
-
-    # build message
-    options = {:subject => "Welcome to What'sHappening!", :phonenumber => "7406410248", :message => "We'd like to get you up and running but first we have 3 simple questions just reply via text\n1. What is your name?\n2. What is your zipcode?\n3. How old is your child?\n4. What is your child interested in? Reply with either 'Arts','Sports', or 'Volunteering'. You will be receiving event texts starting this weekend"}
-
-    options = {:subject => "Welcome to What'sHappening!", :phonenumber => "7406410248", :message => "Test"}
-
-    # send text
-    Eztexting::Sms.single(options)
-
-  end
 end
