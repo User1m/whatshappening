83bbea6709893f75e1c0a85e077df8caec0a5363
Claudius Mbemba <mbembac@gmail.com>
2015-04-18 20:06:05 -0400

    added getting messaging need to decide what to do with it

--- app/controllers/ez_messages_controller.rb
+++ app/controllers/ez_messages_controller.rb
@@ -1,22 +1,26 @@
 require 'net/http'
+require "open-uri"
 
 class EzMessagesController < ApplicationController
 
   def index
 
   # http get all message.
-  # url = URI.parse('https://app.eztexting.com/incoming-messages?format=json&User='<< ENV['EZUSERNAME'] << '&Password='<< ENV['EZPASSWORD'])
-
-   url = URI.parse('https://app.eztexting.com/incoming-messages?format=json&User=user1m&Password=gbH@ck')
-  req = Net::HTTP::Get.new(url.to_s)
-  res = Net::HTTP.start(url.host, 443) {|http|
-    http.request(req)
-  }
-  puts res.body
-
+  uri = URI.parse("https://app.eztexting.com/incoming-messages?format=json&User=#{ENV["EZUSERNAME"]}&Password=#{ENV["EZPASSWORD"]}")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+  @data = http.get(uri.request_uri)
+  @data = @data.body
 end
 
-def getOneMessage
+def find
+   uri = URI.parse("https://app.eztexting.com/incoming-messages/15468266?format=json&User=#{ENV["EZUSERNAME"]}&Password=#{ENV["EZPASSWORD"]}")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+  @data = http.get(uri.request_uri)
+  @data = @data.body
 end
 
 
