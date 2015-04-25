6ee3dccadacef5cbd040eddd2402ca18b7cadd30
Jae Lee <jae@dustral.com>
2015-04-24 21:40:18 -0400

    Move text logic from controller to User model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,23 +10,7 @@ class WelcomeController < ApplicationController
       redirect_to success_path
 
       unless @user.phone_no.nil?
-        @client = Twilio::REST::Client.new ENV['TWILIOSID'], ENV['TWILIOTOKEN']
-
-        body = "Welcome to What's Happening! " +
-          "Messaging and Data rates may apply.\n" +
-          "Here are some events that are happening soon!\n\n"
-
-        Event.last_three.each do |event|
-          body = body + event.to_string + "\n\n"
-        end
-
-        body = body + "Powered by GiveBackHack.\nhttp://givebackhack.com/"
-
-        @client.messages.create(
-          from: ENV['TWILIOPHONE'],
-          to: @user.phone_no,
-          body: body
-        )
+        @user.send_text
       end
     else
       redirect_to success_path, notice: "There was an error. Please try again."
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   validates :zipcode, :length => {:is => 5, :message => "Zip code must be 5 digits long."}, allow_blank: true
   validates :phone_no, :length => {:is => 10, :message => "Please enter a valid phone number."}, presence: true
   validates :phone_no, uniqueness: true
+
+  def send_text
+    @client = Twilio::REST::Client.new ENV['TWILIOSID'], ENV['TWILIOTOKEN']
+
+    body = "Welcome to What's Happening! " +
+      "Messaging and Data rates may apply.\n" +
+      "Here are some events that are happening soon!\n\n"
+
+    Event.last_three.each do |event|
+      body = body + event.to_string + "\n\n"
+    end
+
+    body = body + "Powered by GiveBackHack.\nhttp://givebackhack.com/"
+
+    @client.messages.create(
+      from: ENV['TWILIOPHONE'],
+      to: self.phone_no,
+      body: body
+    )
+  end
 end
