c70b31adfe828fc9f380bae2f0b6802188383a77
Jae Lee <jae@dustral.com>
2015-04-26 19:40:44 -0400

    Vagraintfile and provisioner for faster developing

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt-get -y install git
+sudo apt-get -y install make
+
+git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bash_profile
+echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bash_profile
+
+source ~/.bash_profile
+
+rbenv install 2.2.2
+rbenv global 2.2.2
+cd /vagrant
+gem install bundle
+bundle install --without production
